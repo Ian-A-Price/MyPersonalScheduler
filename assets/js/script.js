4d5eeea2baaf0b615fec4324579734fb584782a1
IanAdamPrice <126291996+Ian-A-Price@users.noreply.github.com>
2023-04-05 16:15:28 -0400

    creating groundwork for JS

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+var tasks = {};
+
+// Create Task Function that accepts Text, Date, and List as parameters
+var createTask = function(taskText, taskDate, taskList) {
+  // Create elements that make up a task item
+  var taskLi = $("<li>").addClass("list-group-item");
+
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+
+  var taskP = $("<p>").addClass("m-1").text(taskText);
+
+  // Append span and p element to parent li
+  taskLi.append(taskSpan, taskP);
+
+  //check due date
+  auditTask(taskLi);
+
+  // append to ul list on the page
+  $("#list-" + taskList).append(taskLi);
+};
+
+var loadTasks = function() {
+  tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  // If local storage is empty, create a new object to track all the tasks
+  if (!tasks) {
+    tasks = {
+      toDo: [],
+      inProgress: [],
+      inReview: [],
+      done: []
+    };
+  }
+
+  // Loop over object properties
+  $.each(tasks, function(list, arr) {
+    console.log(list,arr);
+    //Loop subarray
+    arr.forEach(function(task) {
+      createTask(task.text, task.date, list);
+    });
+  });
+};
+
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
